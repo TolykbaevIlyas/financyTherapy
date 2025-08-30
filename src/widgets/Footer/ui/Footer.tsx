a2fbe2bffd97bf0d6e1fc0ceba569d47874ab711
Saedakhram <102800804+Saedddd@users.noreply.github.com>
2025-08-30 14:20:17 +0600

    ref: header

--- src/widgets/Footer/ui/Footer.tsx
+++ src/widgets/Footer/ui/Footer.tsx
@@ -11,42 +11,50 @@ const Footer = () => {
             <div className="flex flex-col lg:flex-row gap-[32px] lg:gap-0  justify-between items-center text-start my-[64px]">
               <ul className="max-w-[500px] w-full flex flex-col gap-[12px]">
                 <li className="text-[14px] font-[400px] text-[#717680] mb-[5px]">
-                  Мы в социальных сетях
+                  <a href="">Мы в социальных сетях</a>
                 </li>
                 <li className="text-[16px] font-bold text-[#535862]">
-                  Instagram (личные истории и кейсы)
+                  <a href="">Instagram (личные истории и кейсы)</a>
                 </li>
                 <li className="text-[16px] font-bold text-[#535862]">
-                  Telegram (ежедневные советы и новости)
+                  <a href="">Telegram (ежедневные советы и новости)</a>
                 </li>
                 <li className="text-[16px] font-bold text-[#535862]">
-                  YouTube (разборы и обучение)
+                  <a href="">YouTube (разборы и обучение)</a>
                 </li>
-                <li className="text-[16px] font-bold text-[#535862]">TikTok (короткие советы)</li>
                 <li className="text-[16px] font-bold text-[#535862]">
-                  VK (оф. сообщество Центра снижения кредитной нагрузки №1)
+                  <a href="">TikTok (короткие советы)</a>
                 </li>
-                <li className="text-[16px] font-bold text-[#535862]">Дзен (статьи и истории)</li>
+                <li className="text-[16px] font-bold text-[#535862]">
+                  <a href="">VK (оф. сообщество Центра снижения кредитной нагрузки №1)</a>
+                </li>
+                <li className="text-[16px] font-bold text-[#535862]">
+                  <a href="">Дзен (статьи и истории)</a>
+                </li>
               </ul>
               <ul className="max-w-[495px] w-full flex flex-col gap-[17px]">
-                <li className="text-[14px] font-[400px] text-[#717680]">Служба поддержки</li>
+                <li className="text-[14px] font-[400px] text-[#717680]">
+                  <a href="">Служба поддержки</a>
+                </li>
                 <div className="flex flex-col gap-[12px] mb-[30px] ">
                   <li className="text-[16px] font-[400px] text-[#535862]">
-                    Круглосуточная экстренная помощь:
+                    <a href="">Круглосуточная экстренная помощь:</a>
                     <li className="text-[16px] font-[400px] text-[#535862]">
-                      WhatsApp | Telegram | VK | MAX
+                      <a href="">WhatsApp | Telegram | VK | MAX</a>
                     </li>
                   </li>
                   <li className="text-[16px] font-[400px] text-[#535862]">
-                    Консультации и диагностика: Ежедневно с 8:00 до 23:00 МСК
+                    <a href="">Консультации и диагностика:</a> Ежедневно с 8:00 до 23:00 МСК
                   </li>
                   <li className="text-[16px] font-[400px] text-[#535862]">
-                    Техническая поддержка:cskn24.7@gmail.com
+                    <a href="">Техническая поддержка:</a> cskn24.7@gmail.com
                   </li>
                 </div>
-                <li className="text-[14px] font-[400px] text-[#717680]">Юридические данные</li>
+                <li className="text-[14px] font-[400px] text-[#717680]">
+                  <a href="">Юридические данные</a>
+                </li>
                 <li className="text-[16px] font-[400px] text-[#535862]">
-                  ООО ЦСКН1 ИНН: 1900013283 ОГРНИП: 1241900000605
+                  <a href="">ООО ЦСКН1 ИНН: 1900013283 ОГРНИП: 1241900000605</a>
                 </li>
               </ul>
             </div>
@@ -57,12 +65,14 @@ const Footer = () => {
               <div className="text-start">
                 <ul className="flex lg:flex-row flex-col gap-[12px] lg:gap-[24px]">
                   <li className="text-[16px] font-[400px] text-[#717680]">
-                    Согласие на обработку персональных данных
+                    <a href="">Согласие на обработку персональных данных</a>
                   </li>
                   <li className="text-[16px] font-[400px] text-[#717680]">
-                    Политика обработки персональных данных
+                    <a href="">Политика обработки персональных данных</a>
                   </li>
-                  <li className="text-[16px] font-[400px] text-[#717680]">Договор-оферта</li>
+                  <li className="text-[16px] font-[400px] text-[#717680]">
+                    <a href="">Договор-оферта</a>
+                  </li>
                 </ul>
               </div>
             </div>
--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
     <>
       <header className="w-full">
         <ContainerWrapper>
-          <div className="flex justify-between px-[14px] items-center py-[13px] rounded-2xl border-1 w-full border-gray-200 mt-[24px]">
+          <div className="flex justify-between px-[14px] items-center py-[13px] rounded-2xl border-0 lg:border-1 w-full border-gray-200  lg:mt-[24px]">
             <img src={logo} alt="Logo" />
             <div className="max-w-[177px] max-h-[40px] h-full w-full">
               <Button className="w-full" type="primary">
